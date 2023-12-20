24a950243f40288e57dbc1219f9d78c858389d15
Md Asadul Islam Nayeem <asadulnayeem99@gmail.comm>
2023-12-21 00:25:41 +0600

    new

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import React from "react";
+import Layout from "./components/Layout";
+import Home from "./components/pages/Home";
+import "./styles/App.css";
+
+const App = () => {
+  return (
+    <>
+      <Layout>
+        <Home />
+      </Layout>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Video from "../Videos";
+
+const Home = () => {
+  return (
+    <>
+      <Video />
+    </>
+  );
+};
+
+export default Home;
